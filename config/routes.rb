1cbae231d7216f172689c977bd9cebe636844d3d
Daniel Warner Smith <sendto.dws@gmail.com>
2013-06-15 23:23:44 -0700

    added a users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+
+  match '/help', to: 'users#help'
+  match '/about', to: 'users#about'
+  match '/contact', to: 'users#contact'
 
   match '/help',       to: 'static_pages#help'
   match '/about',      to: 'static_pages#about'
